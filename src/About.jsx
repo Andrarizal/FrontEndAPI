25b11f5da05e739cabb197100f298caaf7de8026
Andrarizal <72431050+Andrarizal@users.noreply.github.com>
2023-01-30 17:21:51 +0700

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -43,7 +43,7 @@ export const About = () => {
             Using Framework With React JS and Bootstrap 4
           </li>
         <div className="my-3">
-          You can download this project  <a target="_blank" href="https://github.com/narantyomaulana/tubes-frontend-narantyo" className='text-warning'>here</a>
+          You can download this project  <a target="_blank" href="https://github.com/Andrarizal/FrontEndAPI.git" className='text-warning'>here</a>
         </div>
         Thanks!
       </div>
